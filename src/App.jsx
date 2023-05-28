fa544b05c02355f3adfa7d6d4299a005c92d75c9
RikuDev <riku@rikudev.com>
2023-05-28 19:11:48 +0200

    skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 
 import Home from './Pages/Home.jsx';
 import Navbar from './Pages/Navbar.jsx';
-import Test1 from './Pages/test1.jsx';
-import Test2 from './Pages/test2.jsx';
-import Test3 from './Pages/test3.jsx';
-
+import Skills from './Pages/Skills.jsx';
 
 export default function App() {
   
@@ -13,9 +10,7 @@ export default function App() {
     
         <Navbar />
         <Home />
-        <Test1 />
-        <Test2 />
-        <Test3 />  
+        <Skills />
     </div>
   );
 }
---
+++ src/Pages/Skills.jsx
@@ -0,0 +1,44 @@
+import html from '../assets/img/html.svg'
+import css from '../assets/img/css.svg'
+import javascript from '../assets/img/javascript.svg'
+import react from '../assets/img/react.svg'
+import sass from '../assets/img/sass.svg'
+import tailwind from '../assets/img/tailwind.svg'
+import vscode from '../assets/img/vscode.svg'
+import vite from '../assets/img/vite.svg'
+import git from '../assets/img/git.svg'
+import github from '../assets/img/github.svg'
+
+
+export default function Skills(){
+
+    
+    return (
+        <section id="skills" className="text-xl w-screen items-center flex pt-52 z-20">
+        <div className="flex flex-col justify-between mt-10 max-w-[1280px] w-screen mx-auto items-center maxsm:flex-col maxsm:gap-10">
+            <div className="flex flex-col font-medium gap-2 justify-center items-center">
+                <span className="text-2xl text-center">My Skills</span>
+                <span className="text-gray-500 text-center"> Technologies I´ve been working with recently</span>
+            </div>
+
+            <div className='flex flex-wrap justify-between items-center mx-auto w-full mt-10 gap-10'>
+                <img className='w-20 h-20' src={html} alt='html'></img>
+                <img className='w-20 h-20' src={css} alt='css'></img>
+                <img className='w-20 h-20' src={javascript} alt='javascript'></img>
+                <img className='w-20 h-20' src={react} alt='react'></img>
+                <img className='w-20 h-20' src={tailwind} alt='tailwind'></img>
+                <img className='w-20 h-20' src={sass} alt='sass'></img>
+                <img className='w-20 h-20' src={vscode} alt='vscode'></img>
+                <img className='w-20 h-20' src={vite} alt='vite'></img>
+                <img className='w-20 h-20' src={git} alt='git'></img>
+                <img className='w-20 h-20' src={github} alt='github'></img>
+                
+            </div>
+
+            
+        </div>
+        </section>
+    );
+}
+
+// flex justify-between items-center max-w-[1280px] w-screen  mx-auto
