2e255cecb5bd44c8dd3438f630b942e39b7380d1
RikuDev <riku@rikudev.com>
2023-05-28 09:00:42 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
 import Home from './Pages/Home.jsx';
 import Navbar from './Pages/Navbar.jsx';
+import Test1 from './Pages/test1.jsx';
+import Test2 from './Pages/test2.jsx';
+import Test3 from './Pages/test3.jsx';
 
 
 export default function App() {
@@ -10,11 +13,9 @@ export default function App() {
     
         <Navbar />
         <Home />
-        <Home />
-        <Home />
-        <Home />
-        <Home />
-  
+        <Test1 />
+        <Test2 />
+        <Test3 />  
     </div>
   );
 }
--- src/assets/utilities/navbarDataEs.js
+++ src/assets/utilities/navbarDataEs.js
@@ -1,25 +1,25 @@
 export const navbarDataEs = [
     {
         title: 'Inicio',
-        href: '/home',
+        href: '#home',
         cname:
         'border-t font-medium w-screen flex justify-center p-2.5 mt-3 minlg:border-none minlg:p-0 minlg:mt-0 minlg:w-auto',
     },
     {
         title: 'Sobre mi',
-        href: '.about',
+        href: '#test1',
         cname:
         'border-t font-medium w-screen flex justify-center p-2.5 minlg:border-none minlg:p-0 minlg:w-auto',
     },
     {
         title: 'Habilidades',
-        href: '.skills',
+        href: '#test2',
         cname:
         'border-t font-medium w-screen flex justify-center p-2.5 minlg:border-none minlg:p-0 minlg:w-auto',
     },
     {
         title: 'Proyectos',
-        href: '.projects',
+        href: '#test3',
         cname:
         'border-t border-b font-medium w-screen flex justify-center p-2.5 minlg:border-none minlg:p-0 minlg:w-auto',
     },
