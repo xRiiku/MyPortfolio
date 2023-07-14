a9c1bbeef3193284f1e3e7258d4c5a926cc3b395
RikuDev <riku@rikudev.com>
2023-07-14 16:43:34 +0200

    home changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,10 +8,9 @@ export default function Home(){
         <main id="home" className="text-xl w-screen items-center flex pt-52 z-20 px-10">
         <div className="flex mt-10 justify-between items-center max-w-[1280px] w-screen mx-auto maxsm:flex-col maxsm:gap-10">
             <div className="text-4xl flex flex-col font-medium gap-2 animated-leftSlide">
-                <span>{t('home.hi')}</span>
-                <span>{t('home.nameis')}</span>
-                <span className="bg-gradient-to-r from-sky-500 via-purple-500 to-pink-600 text-transparent bg-clip-text">Daniel RC</span>
-                <span>{t('home.junior')}</span>
+                <p className='text-xl opacity-80'>{t('home.hi')}</p>                
+                <p className="text-8xl bg-gradient-to-r from-sky-500 via-purple-500 to-pink-600 text-transparent bg-clip-text">Daniel RC</p>
+                <p>{t('home.developer')}</p>
             </div>
 
             <div className='border-4 bg-gradient-to-b from-sky-500 via-purple-500 to-pink-600 bg-clip-border border-transparent rounded-full animated-rightSlide'>
@@ -23,5 +22,3 @@ export default function Home(){
         </main>
     );
 }
-
-// flex justify-between items-center max-w-[1280px] w-screen  mx-auto
